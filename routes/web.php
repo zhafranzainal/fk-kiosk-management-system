ce07575a76b37f02acfa8acb6b322f4a7da334b2
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-12-09 04:58:48 +0800

    Add routes for each module

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\ComplaintController;
+use App\Http\Controllers\KioskController;
 use App\Http\Controllers\SaleController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,13 +25,16 @@ Route::get('/', function () {
 
 Route::resource('/manage-sales', SaleController::class);
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-  
+
+    Route::resource('users', UserController::class);
+    Route::resource('kiosks', KioskController::class);
+    Route::resource('applications', ApplicationController::class);
+    Route::resource('sales', SaleController::class);
+    Route::resource('transactions', TransactionController::class);
+    Route::resource('complaints', ComplaintController::class);
 });
