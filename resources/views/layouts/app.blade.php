f94db4d8b7d0d70f11bf1650c7f415f29aeec970
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-12-10 05:04:58 +0800

    Dynamically change theme based on user role

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,14 +25,31 @@
 
 </head>
 
-<body class="loading"
-    data-layout-config='{"leftSideBarTheme":"default","layoutBoxed":false, "leftSidebarCondensed":false, "leftSidebarScrollable":false,"darkMode":false, "showRightSidebarOnStart": true}'>
+@if (auth()->user()->getRoleNames()->first() == 'Admin')
 
-    <div class="wrapper">
+    <body class="loading"
+        data-layout-config='{"leftSideBarTheme":"light","layoutBoxed":false, "leftSidebarCondensed":false, "leftSidebarScrollable":false,"darkMode":false, "showRightSidebarOnStart": true}'>
+    @elseif(auth()->user()->getRoleNames()->first() == 'Kiosk Participant')
 
-        <div class="left-side-menu">
+        <body class="loading"
+            data-layout-config='{"leftSideBarTheme":"default","layoutBoxed":false,"leftSidebarCondensed":false,"leftSidebarScrollable":false,"darkMode":false,"showRightSidebarOnStart":true}'>
+@endif
 
-            <!-- LOGO -->
+<div class="wrapper">
+
+    <div class="left-side-menu">
+
+        <!-- LOGO -->
+        @if (auth()->user()->getRoleNames()->first() == 'Admin')
+            <a href="index.html" class="logo text-center logo-dark">
+                <span class="logo-lg">
+                    <img src="{{ asset('assets/images/logo-dark.png') }}" alt="" height="16">
+                </span>
+                <span class="logo-sm">
+                    <img src="{{ asset('assets/images/logo_sm_dark.png') }}" alt="" height="16">
+                </span>
+            </a>
+        @elseif(auth()->user()->getRoleNames()->first() == 'Kiosk Participant')
             <a href="index.html" class="logo text-center logo-light">
                 <span class="logo-lg">
                     <img src="{{ asset('assets/images/logo.png') }}" alt="" height="16">
@@ -41,174 +58,196 @@
                     <img src="{{ asset('assets/images/logo_sm.png') }}" alt="" height="16">
                 </span>
             </a>
+        @endif
 
-            <div class="h-100" id="left-side-menu-container" data-simplebar>
+        <div class="h-100" id="left-side-menu-container" data-simplebar>
 
-                <ul class="metismenu side-nav">
+            <ul class="metismenu side-nav">
 
-                    {{-- Profile and Role --}}
-                    <div class="side-nav-link">
-                        <a href="javascript: void(0);">
-                            <img src="{{ asset('assets/images/users/avatar-1.jpg') }}" alt="user-image" height="35"
-                                class="rounded-circle shadow-sm">
+                {{-- Profile and Role --}}
+                <div class="side-nav-link">
+                    <a href="javascript: void(0);">
+
+                        <img src="{{ asset('assets/images/users/avatar-1.jpg') }}" alt="user-image" height="35"
+                            class="rounded-circle shadow-sm">
+
+                        @if (auth()->user()->getRoleNames()->first() == 'Admin')
+                            <div class="user-avatar">
+                                <span class="leftbar-user-name text-dark"
+                                    style="font-weight: bold">{{ Auth::user()->name }}</span>
+                                <p class="text-muted font-14">{{ auth()->user()->getRoleNames()->first() }}</p>
+                            </div>
+                        @elseif(auth()->user()->getRoleNames()->first() == 'Kiosk Participant')
                             <div class="user-avatar">
                                 <span class="leftbar-user-name text-white"
                                     style="font-weight: bold">{{ Auth::user()->name }}</span>
                                 <p class="text-white font-14">{{ auth()->user()->getRoleNames()->first() }}</p>
                             </div>
-                        </a>
-                    </div>
+                        @endif
 
-                    <li class="side-nav-title side-nav-item">Menu</li>
-
-                    @include('navigation-menu')
-
-                </ul>
-
-                <div class="clearfix"></div>
-
-            </div>
-
-        </div>
-
-        <div class="content-page">
-
-            <div class="content">
-
-                {{-- Navigation Bar --}}
-                <div class="navbar-custom" style="background-color: #CA3433;">
-
-                    <ul class="list-unstyled topbar-right-menu float-right mb-0">
-
-                        {{-- Notification --}}
-                        <li class="dropdown notification-list">
-
-                            <a class="nav-link dropdown-toggle arrow-none" data-toggle="dropdown" href="#"
-                                role="button" aria-haspopup="false" aria-expanded="false">
-                                <i class="dripicons-bell noti-icon text-white"></i>
-                                <span class="noti-icon-badge"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right dropdown-menu-animated dropdown-lg">
-
-                                <div class="dropdown-item noti-title">
-                                    <h5 class="m-0">
-                                        <span class="float-right">
-                                            <a href="javascript: void(0);" class="text-dark">
-                                                <small>Clear All</small>
-                                            </a>
-                                        </span>
-                                        Notification
-                                    </h5>
-                                </div>
-
-                                <div style="max-height: 230px;" data-simplebar>
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                                        <div class="notify-icon bg-primary">
-                                            <i class="mdi mdi-comment-account-outline"></i>
-                                        </div>
-                                        <p class="notify-details">Caleb Flakelar commented on Admin
-                                            <small class="text-muted">1 min ago</small>
-                                        </p>
-                                    </a>
-                                </div>
-
-                                <a href="javascript:void(0);"
-                                    class="dropdown-item text-center text-primary notify-item notify-all">
-                                    View All
-                                </a>
-
-                            </div>
-
-                        </li>
-
-                        {{-- Profile --}}
-                        <li class="dropdown notification-list">
-
-                            <a class="nav-link dropdown-toggle nav-user arrow-none mr-0" data-toggle="dropdown"
-                                href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                                <span class="account-user-avatar">
-                                    <img src="{{ asset('assets/images/users/avatar-1.jpg') }}" alt="user-image"
-                                        class="rounded-circle">
-                                </span>
-                            </a>
-
-                            <div
-                                class="dropdown-menu dropdown-menu-right dropdown-menu-animated topbar-dropdown-menu profile-dropdown">
-
-                                <a href="{{ route('profile.show') }}" class="dropdown-item notify-item">
-                                    <i class="mdi mdi-account-circle mr-1"></i>
-                                    {{ __('My Profile') }}
-                                </a>
-
-                                <!-- Authentication -->
-                                <form method="POST" action="{{ route('logout') }}"
-                                    class="dropdown-item notify-item">
-                                    @csrf
-                                    <i class="mdi mdi-logout mr-1"></i>
-                                    <a href="{{ route('logout') }}"
-                                        onclick="event.preventDefault(); this.closest('form').submit();">
-                                        {{ __('Log Out') }}
-                                    </a>
-                                </form>
-
-                            </div>
-
-                        </li>
-
-                    </ul>
-
-                    <button class="button-menu-mobile open-left disable-btn text-white">
-                        <i class="mdi mdi-menu"></i>
-                    </button>
-
+                    </a>
                 </div>
 
-                {{-- Page Content --}}
-                <div class="container-fluid">
-                    {{ $slot }}
-                </div>
+                <li class="side-nav-title side-nav-item">Menu</li>
 
-            </div>
+                @include('navigation-menu')
 
-            <footer class="footer">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <script>
-                                document.write(new Date().getFullYear())
-                            </script> © FKKMS
-                        </div>
-                    </div>
-                </div>
-            </footer>
+            </ul>
+
+            <div class="clearfix"></div>
 
         </div>
 
     </div>
 
-    <!-- bundle -->
-    <script src="{{ asset('assets/js/vendor.min.js') }}"></script>
-    <script src="{{ asset('assets/js/app.min.js') }}"></script>
+    <div class="content-page">
 
-    <!-- third party js -->
-    <script src="{{ asset('assets/js/vendor/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('assets/js/vendor/dataTables.bootstrap4.js') }}"></script>
-    <script src="{{ asset('assets/js/vendor/dataTables.responsive.min.js') }}"></script>
-    <script src="{{ asset('assets/js/vendor/responsive.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('assets/js/vendor/dataTables.checkboxes.min.js') }}"></script>
+        <div class="content">
 
-    <!-- demo app -->
-    <script src="{{ asset('assets/js/pages/demo.customers.js') }}"></script>
+            {{-- Navigation Bar --}}
+            @if (auth()->user()->getRoleNames()->first() == 'Admin')
+                <div class="navbar-custom">
+                @elseif(auth()->user()->getRoleNames()->first() == 'Kiosk Participant')
+                    <div class="navbar-custom" style="background-color: #CA3433;">
+            @endif
 
-    <!-- Datatables js -->
-    <script src="{{ asset('assets/js/vendor/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('assets/js/vendor/dataTables.bootstrap4.js') }}"></script>
-    <script src="{{ asset('assets/js/vendor/dataTables.responsive.min.js') }}"></script>
-    <script src="{{ asset('assets/js/vendor/responsive.bootstrap4.min.js') }}"></script>
+            <ul class="list-unstyled topbar-right-menu float-right mb-0">
 
-    <!-- Datatable Init js -->
-    <script src="{{ asset('assets/js/pages/demo.datatable-init.js') }}"></script>
+                {{-- Notification --}}
+                <li class="dropdown notification-list">
+
+                    <a class="nav-link dropdown-toggle arrow-none" data-toggle="dropdown" href="#" role="button"
+                        aria-haspopup="false" aria-expanded="false">
+
+                        @if (auth()->user()->getRoleNames()->first() == 'Admin')
+                            <i class="dripicons-bell noti-icon text-dark"></i>
+                        @elseif(auth()->user()->getRoleNames()->first() == 'Kiosk Participant')
+                            <i class="dripicons-bell noti-icon text-white"></i>
+                        @endif
+
+                        <span class="noti-icon-badge"></span>
+
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-animated dropdown-lg">
+
+                        <div class="dropdown-item noti-title">
+                            <h5 class="m-0">
+                                <span class="float-right">
+                                    <a href="javascript: void(0);" class="text-dark">
+                                        <small>Clear All</small>
+                                    </a>
+                                </span>
+                                Notification
+                            </h5>
+                        </div>
+
+                        <div style="max-height: 230px;" data-simplebar>
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <div class="notify-icon bg-primary">
+                                    <i class="mdi mdi-comment-account-outline"></i>
+                                </div>
+                                <p class="notify-details">Caleb Flakelar commented on Admin
+                                    <small class="text-muted">1 min ago</small>
+                                </p>
+                            </a>
+                        </div>
+
+                        <a href="javascript:void(0);"
+                            class="dropdown-item text-center text-primary notify-item notify-all">
+                            View All
+                        </a>
+
+                    </div>
+
+                </li>
+
+                {{-- Profile --}}
+                <li class="dropdown notification-list">
+
+                    <a class="nav-link dropdown-toggle nav-user arrow-none mr-0" data-toggle="dropdown"
+                        href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                        <span class="account-user-avatar">
+                            <img src="{{ asset('assets/images/users/avatar-1.jpg') }}" alt="user-image"
+                                class="rounded-circle">
+                        </span>
+                    </a>
+
+                    <div
+                        class="dropdown-menu dropdown-menu-right dropdown-menu-animated topbar-dropdown-menu profile-dropdown">
+
+                        <a href="{{ route('profile.show') }}" class="dropdown-item notify-item">
+                            <i class="mdi mdi-account-circle mr-1"></i>
+                            {{ __('My Profile') }}
+                        </a>
+
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}" class="dropdown-item notify-item">
+                            @csrf
+                            <i class="mdi mdi-logout mr-1"></i>
+                            <a href="{{ route('logout') }}"
+                                onclick="event.preventDefault(); this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </a>
+                        </form>
+
+                    </div>
+
+                </li>
+
+            </ul>
+
+            <button class="button-menu-mobile open-left disable-btn text-white">
+                <i class="mdi mdi-menu"></i>
+            </button>
+
+        </div>
+
+        {{-- Page Content --}}
+        <div class="container-fluid">
+            {{ $slot }}
+        </div>
+
+    </div>
+
+    <footer class="footer">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script> © FKKMS
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</div>
+
+</div>
+
+<!-- bundle -->
+<script src="{{ asset('assets/js/vendor.min.js') }}"></script>
+<script src="{{ asset('assets/js/app.min.js') }}"></script>
+
+<!-- third party js -->
+<script src="{{ asset('assets/js/vendor/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/dataTables.bootstrap4.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/dataTables.checkboxes.min.js') }}"></script>
+
+<!-- demo app -->
+<script src="{{ asset('assets/js/pages/demo.customers.js') }}"></script>
+
+<!-- Datatables js -->
+<script src="{{ asset('assets/js/vendor/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/dataTables.bootstrap4.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/responsive.bootstrap4.min.js') }}"></script>
+
+<!-- Datatable Init js -->
+<script src="{{ asset('assets/js/pages/demo.datatable-init.js') }}"></script>
 
 </body>
 
